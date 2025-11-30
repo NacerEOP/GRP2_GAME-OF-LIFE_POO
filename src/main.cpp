9bf20c7be5e515451a3553512b5f9d76e0d52d79
leith-allah <leithallah.contact@gmail.com>
2025-11-30 22:32:14 +0100

    Rectification du Main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 #include "Services/GameService.h"
 
 int main() {
-	GameService* service = createDefaultGameService();
-	ConsoleUI ui(*service);
-	ui.run();
-	delete service;
-	return 0;
+    GameService* service = createDefaultGameService();
+    service->setGridSize(GridSize::SMALL); // <--- set small grid at startup
+    ConsoleUI ui(*service);
+    ui.run();
+    delete service;
+    return 0;
 }
