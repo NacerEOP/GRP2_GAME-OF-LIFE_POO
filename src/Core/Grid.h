a5bca7c4e3bed0bc9b56d175ee362e815c8f3969
MAC <MAC@MacBook-Air.local>
2025-11-30 15:35:53 -0800

    a

--- src/Core/Grid.h
+++ src/Core/Grid.h
@@ -1,20 +1,29 @@
-#include <iostream>
+#include <vector>
 #pragma once
 
 
 class Grid {
 
 private:
+
     int rows;
     int cols;
+    std::vector<std::vector<bool>> cells;
 
 public:
-    Grid(int r, int c) ;
+    
+    Grid(int r, int c);
     ~Grid();
+
     int getR() const;
     int getC() const;
+
     void setR(int r);
     void setC(int c);
+
+    bool getCell(int x, int y) const;       // retourne l'état d'une cellule
+    void setCell(int x, int y, bool state); // modifie l'état d'une cellule
+    void print() const;                     // affichage console pour test
 
 };
 
