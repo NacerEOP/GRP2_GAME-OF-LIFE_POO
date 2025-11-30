f5867e195dd0376fc4cf401f8c07661cbfc578e4
MAC <MAC@MacBook-Air.local>
2025-11-30 15:57:51 -0800

    class Grid

--- src/Core/Grid.cpp
+++ src/Core/Grid.cpp
@@ -1,28 +1,27 @@
 #include "Grid.h"
 #include <iostream>
 
-// constructeur
+
 Grid::Grid(int r, int c) : rows(r), cols(c) {
     cells = std::vector<std::vector<bool>>(rows, std::vector<bool>(cols, false));
 }
 
-// destructeur
+
 Grid::~Grid() {}
 
-// Getters
+
 int Grid::getR() const { return rows; }
 int Grid::getC() const { return cols; }
 
-// Setters
 void Grid::setR(int r) { rows = r; }
 void Grid::setC(int c) { cols = c; }
 
-// obtenir état d'une cellule
+
 bool Grid::getCell(int x, int y) const {
     return cells[x][y];
 }
 
-// modifier état d'une cellule
+
 void Grid::setCell(int x, int y, bool state) {
     cells[x][y] = state;
 }
